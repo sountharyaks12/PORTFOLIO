78be00ce1c940ed957d7ca7e6b76cf01f139a929
sountharya <sountharyaks@gmail.com>
2024-12-30 12:28:13 +0530

    pic2

--- src/NavBar/Home.jsx
+++ src/NavBar/Home.jsx
@@ -353,7 +353,7 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="w-full lg:w-2/3 flex justify-center">
+            <div className="w-full h-40 lg:w-2/3 flex justify-center">
               <img src="i.JPG" alt="Sountharya" className="rounded-full w-40 h-40 object-cover" />
             </div>
           </div>
