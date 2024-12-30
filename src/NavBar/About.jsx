d9ac9807c7771b90b609335b885bd39fe9b66880
sountharya <sountharyaks@gmail.com>
2024-12-30 09:58:25 +0530

    mt

--- src/NavBar/About.jsx
+++ src/NavBar/About.jsx
@@ -19,7 +19,7 @@ import React from 'react';
 
 export default function About() {
     return (
-        <div id='about' className='min-h-screen text-center m-4 md:m-16'>
+        <div id='about' className='min-h-screen text-center m-4 mt-28 md:m-16'>
             <div className='border-2 border-red-950 rounded-lg p-5 shadow-lg'>
                 <h2 className='text-3xl mt-10'>About Me</h2>
                 <ul className='list-disc m-6 text-start'>
