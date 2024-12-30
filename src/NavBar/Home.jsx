7a1c58ff5933f9979c34b549aa2d707c6c03a22d
sountharya <sountharyaks@gmail.com>
2024-12-30 10:14:13 +0530

    kj

--- src/NavBar/Home.jsx
+++ src/NavBar/Home.jsx
@@ -154,7 +154,7 @@ export default function Home() {
       </div>
 
       <div className="flex items-center h-[82vh] animate-fadeInUp sm:animate-none">
-        <div id="home" className="w-full mt-10">
+        <div id="home" className="w-full mt-20">
           <div className="flex justify-center flex-col-reverse lg:flex-row md:flex-col-reverse sm:flex-col-reverse xs:flex-col-reverse sm:mt-36 xs:mt-36 leading-loose items-center w-screen my-auto px-5">
             <div className="p-3 w-full lg:w-1/3 animate-slideLeft sm:animate-fadeInUp">
               <div className="text-center p-3 flex flex-wrap justify-center">
