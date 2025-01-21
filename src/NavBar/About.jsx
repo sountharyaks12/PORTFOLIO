fa40f21122f429a2b864de3cd90b1e26038024dd
sountharya <sountharyaks@gmail.com>
2025-01-21 12:10:30 +0530

    list star

--- src/NavBar/About.jsx
+++ src/NavBar/About.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import '../index.css'
 
 export default function About() {
     return (
-        <div id='about' className='min-h-screen text-center m-4 mt-96 md:m-16'>
-            <div className='border-2 border-red-950 rounded-lg p-5 shadow-lg'>
-                <h2 className='text-3xl mt-10'>About Me</h2>
-                <ul className='list-disc m-6 text-start'>
-                    <li className='mt-10'>My journey in tech began with a fascination for how the web works, and it has grown into a full-blown passion for building scalable, interactive, and aesthetically pleasing solutions.</li>
-                    <li>With a focus on crafting impactful solutions, I enjoy turning complex problems into simple, user-friendly designs. During my internship, I honed my ability to develop and deploy full-stack projects</li>
+        <div id='about' className='min-h-screen text-center w-full rounded-lg bg-gray-50  mt-40 md:mt-10'>
+            <div className='p-8'>
+                <h2 className='text-3xl text-gray-900 font-bold mt-8'>About Me</h2>
+
+                <ul className='list-image-custom m-6 text-start space-y-4'>
+                    <li className='mt-5 text-lg text-gray-700 '>My journey in tech began with a fascination for how the web works, and it has grown into a full-blown passion for building scalable, interactive, and aesthetically pleasing solutions.</li>
+                    <li className='text-lg text-gray-700'>With a focus on crafting impactful solutions, I enjoy turning complex problems into simple, user-friendly designs. During my internship, I honed my ability to develop and deploy full-stack projects.</li>
                 </ul>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
-//tailwind.config.js
 import { transform } from 'framer-motion';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html",
